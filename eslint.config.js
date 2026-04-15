817dda2814c91546fcc0f0560b771c34a83244e2
Amedro Louis <142252500+Osiris-Sio@users.noreply.github.com>
2026-04-15 09:22:25 +0200

    feat(header): bouton thème clair/sombre et nouveau logo
    Ce logo (avec un fond vert) permet d'avoir un parfait contraste pour les deux thèmes.

--- eslint.config.js
+++ eslint.config.js
@@ -1,52 +1,52 @@
-import js from "@eslint/js";
-import globals from "globals";
-import html from "eslint-plugin-html";
-import prettierConfig from "eslint-config-prettier";
-import prettierPlugin from "eslint-plugin-prettier";
+import js from '@eslint/js';
+import globals from 'globals';
+import html from 'eslint-plugin-html';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 export default [
   // Configuration de base pour tous les fichiers JS
   {
-    files: ["**/*.js"],
+    files: ['**/*.js'],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: {
         ...globals.browser,
-        ...globals.node,
-      },
+        ...globals.node
+      }
     },
     plugins: {
-      prettier: prettierPlugin,
+      prettier: prettierPlugin
     },
     rules: {
       ...js.configs.recommended.rules,
       ...prettierConfig.rules, // Désactive les règles ESLint qui entrent en conflit avec Prettier
-      "prettier/prettier": "error", // Affiche les problèmes Prettier comme des erreurs ESLint
-      
+      'prettier/prettier': 'error', // Affiche les problèmes Prettier comme des erreurs ESLint
+
       // Règles strictes personnalisées
-      "no-console": "warn",
-      "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
-      "eqeqeq": ["error", "always"], // Force l'utilisation de === et !==
-      "curly": ["error", "all"], // Force les accolades pour les blocs if/else
-      "no-var": "error", // Interdit 'var' (utilise 'let' ou 'const')
-      "prefer-const": "error", // Force 'const' si la variable n'est pas réassignée
-    },
+      'no-console': 'warn',
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      eqeqeq: ['error', 'always'], // Force l'utilisation de === et !==
+      curly: ['error', 'all'], // Force les accolades pour les blocs if/else
+      'no-var': 'error', // Interdit 'var' (utilise 'let' ou 'const')
+      'prefer-const': 'error' // Force 'const' si la variable n'est pas réassignée
+    }
   },
 
   // Support du JS dans les fichiers HTML
   {
-    files: ["**/*.html"],
+    files: ['**/*.html'],
     plugins: {
-      html,
+      html
     },
     settings: {
-      "html/html-extensions": [".html"], 
-    },
+      'html/html-extensions': ['.html']
+    }
   },
 
   // Ignorer certains dossiers
   {
-    ignores: ["node_modules/", "dist/", "vendor/"],
-  },
+    ignores: ['node_modules/', 'dist/', 'vendor/']
+  }
 ];
